70d03d242c1468b94a708a42852a50bdc87404ca
avalenzo1 <94491552+avalenzo1@users.noreply.github.com>
2023-01-12 07:32:35 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -207,7 +207,7 @@ const Atom = (function() {
 
             this.style.lineWidth = (this.ruleSets.usesStroke && this.ruleSets.usesFill) ? this.style.lineWidth * 2 : this.style.lineWidth;
 
-            let bgColor = new Color(255,255,255,0.75);
+            let bgColor = new Color(255,255,255,1);
 //             bgColor.random({ alpha: 0.05 });
 
             setInterval(function() {
